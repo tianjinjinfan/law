a42e90772b51c8a8850a8c9549303bace740a6db
tianjinjinfan <144921482+tianjinjinfan@users.noreply.github.com>
2023-09-14 00:31:08 +0800

    Add files via upload

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,18 +7,24 @@ export default function Footer() {
      <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
       
       <div>
-        作者:台风(chatgpt魔法师 <a
-          href="http://wz.aidaohang.com.cn/taifeng/?f=ailvsuo"
+        玖策AI <a
+          href="https://9cece.net/"
           target="_blank"
           rel="noreferrer"
           className="text-blue-500 hover:text-blue-700 hover:underline transition underline-offset-2"
         >
           了解更多
-        </a>)
+        
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
-       Powered by{" "}
-       ailvsuo.com.cn
+       <a
+          href="https://beian.miit.gov.cn/"
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-500 hover:text-blue-700 hover:underline transition underline-offset-2"
+        >
+       津ICP备2023005288号-3
+	   </a>)
       </div>
     </footer>
   );
